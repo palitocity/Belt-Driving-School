cf4d67094529deb6eae6dba39d1b7235f6965493
Oyebowale Azeez <Oyebowale@relianceinfosystems.com>
2025-10-03 11:36:57 +0100

    added mogons

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  fullname: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
