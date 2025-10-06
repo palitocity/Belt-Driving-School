36bb6cf3abfbc507c2acbb9316c2f4d5f96d6b20
Oyebowale Azeez <Oyebowale@relianceinfosystems.com>
2025-10-07 00:30:37 +0100

    complte instructor dashboard

--- src/pages/instructor/students.tsx
+++ src/pages/instructor/students.tsx
@@ -32,7 +32,7 @@ const StudentsList = () => {
       const id = instructorId ? JSON.parse(instructorId).id : null;
 
       const response = await axios.get(
-        `https://belt-driving-school-backend-3.onrender.com/api/instructor/dashboard/${instructorId}/students`,
+        `https://belt-driving-school-backend-3.onrender.com/api/instructor/dashboard/${id}/students`,
         {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
